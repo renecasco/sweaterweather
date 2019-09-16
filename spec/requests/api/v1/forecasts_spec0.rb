b65f4b9a045da56a30457701ac3b326505c7017e
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-16 06:10:40 -0600

    adds forecasts controller

--- spec/requests/api/v1/forecasts_spec0.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-describe "Forecast API" do
-  it "sends forecast data for a city" do
-    get '/api/v1/forecast?location=denver,co'
-
-    expect(response).to be_successful
-  end
-end
