dbd302d797087aa48c866aadd01718dc238860be
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-09-16 05:10:50 -0600

    adds DarkSkyService

---
+++ app/services/dark_sky_service.rb
@@ -0,0 +1,13 @@
+class DarkSkyService
+
+  def get_forecast(location)
+    conn = Faraday.new(url: "https://api.darksky.net") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/forecast/#{ENV["DARK_SKY_API_KEY"]}/#{location}")
+
+    forecast_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
